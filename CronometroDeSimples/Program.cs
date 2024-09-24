90c24784a374ca33f664c1437c7bb9d482b06641
Sandyla Stocler <sanystocler@gmail.com>
2024-09-24 14:05:25 -0300

    Atualizado

---
+++ CronometroDeSimples/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
+Stopwatch stopwatch = new Stopwatch();
+
+Console.WriteLine("Pressione 's' para iniciar o cronômetro e 'p' para parar.");
+
+while(true)
+{
+    var key = Console.ReadLine(true);
+
+    if (key, keyChar == 's')
+}
+else if (Key.KeyChar == 'p')
+{
+    stopwatch.Stop();
+
+    Console.WriteLine($"Cronômetro parado. Tempo Decorrido: {stopwatch.Elapsed.TotalSeconds} segundos.")
+break;
+}
--- DecisaoDeAlmoco/Program.cs
+++ DecisaoDeAlmoco/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine($"Seu escolher para o almoço: ");
+		Console.WriteLine(Console.ReadLine());
+		string[] restaurantes =
+		{
+			"Cozinha Italiana",
+			"Comida Mexicana",
+			"Restaurante Vegetariano",
+			"Churrascaria",
+			"Comida Sushi",
+			"Comida Chinesa",
+			"Fast Food",
+			"Pizza"
+		};
+		Random random = new Random();
+	}
+}
